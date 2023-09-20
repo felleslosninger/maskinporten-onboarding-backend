15bcbff7e98f37c5851ec950da3ac6a4eb8d499e
Lars Kåre Syversen <lars@Larss-MacBook-Pro.local>
2023-09-20 13:03:56 +0200

    changed proxy to handle fetching open scopes

--- src/main/java/no/digdir/simplifiedonboarding/ProxyEnvironmentController.java
+++ src/main/java/no/digdir/simplifiedonboarding/ProxyEnvironmentController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/{env}/datasharing")
+@RequestMapping("/api/{env}")
 public class ProxyEnvironmentController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProxyEnvironmentController.class);
@@ -38,7 +38,11 @@ public class ProxyEnvironmentController {
         OAuth2AccessToken accessToken = getAccessToken(authentication, servletRequest, servletResponse);
 
         MaskinportenConfig.EnvironmentConfig config = maskinportenConfig.getConfigFor(environment);
+        String queries =servletRequest.getQueryString();
         String uri = config.getApi() + proxy.path("/api/" + config.getEnvironment());
+        if (queries != null) {
+            uri += "?" + queries;
+        }
         logger.info("GET to {}", uri);
         return proxy.uri(uri)
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken.getTokenValue())
@@ -89,7 +93,6 @@ public class ProxyEnvironmentController {
                 })
                 .build();
         OAuth2AuthorizedClient authorizedClient = this.authorizedClientManager.authorize(authorizeRequest);
-
         return authorizedClient.getAccessToken();
     }
 }
